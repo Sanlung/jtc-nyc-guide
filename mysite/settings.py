0966e9408601a9e3a0a54c058e1559bb1d72b9a7
Ash Rahman <ashiq.r31@gmail.com>
2021-10-28 18:29:32 -0400

    reorder app config

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'nyc.apps.NycConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'nyc.apps.NycConfig'
 ]
 
 MIDDLEWARE = [
